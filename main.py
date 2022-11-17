df4764f15f89752f022e2acb95f0a2705af69324
Leonel Carrizo <102891741+leonel-carrizo@users.noreply.github.com>
2022-11-17 21:36:35 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# numero = input('ingresa un valor: ')
+
+
+# while (numero.isnumeric() == False):
+#     print('no es numemero')
+#     numero = input('ingresa un numero: ')
+
+# if numero.isnumeric():
+#     numero = int(numero)
+#     if numero % 2 == 0:
+#         negative = -numero
+#         print(numero, 'es PAR y su negativo es ', negative)
+#     else:
+#         print(numero, 'Es Impar')
+# else:
+#     print('Ha ocurrido un error')
+
+from ast import For
+
+
+lista = ['hola', 'Adios', 'por favor']
+
+print(dir(list))
